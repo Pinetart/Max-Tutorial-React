dd185eee407cf54b459b044a8bbd1c15ef447c60
Zane Birkett <axelbirkett@gmail.com>
2022-03-01 11:43:08 -0400

    Working with "State"

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,21 +1,22 @@
+import React, { useState } from "react";
+
 import ExpenseDate from "./ExpenseDate";
 import ExpenseAmount from "./ExpenseAmount";
-import ExpenseTitle from "./ExpenseTitle";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
-  let title = props.title;
+  const [title, setTitle] = useState(props.title);
 
   const clickHandler = () => {
-    title = "updated!";
+    setTitle("Updated");
     console.log(title);
   };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <ExpenseTitle title={props.title} />
+        <h2>{title}</h2>
         <ExpenseAmount amount={props.amount} />
       </div>
       <button onClick={clickHandler}>Change Title</button>
--- src/components/Expenses/ExpenseTitle.js
+++
@@ -1,7 +0,0 @@
-import "./ExpenseTitle.css";
-
-const ExpenseTitle = (props) => {
-  const title = props.title;
-  return <h2>{title}</h2>;
-};
-export default ExpenseTitle;
