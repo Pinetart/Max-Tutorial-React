912613ddd1b98794ee7e2129e2af4fed302690e6
Zane Birkett <axelbirkett@gmail.com>
2022-05-31 12:57:08 -0400

    Adding a Cart Context

---
+++ src/context/CartProvider.js
@@ -0,0 +1,29 @@
+import React, { createContext } from "react";
+
+export const CartContext = createContext({
+  items: [],
+  totalAmount: 0,
+  addItem: () => {},
+  removeItem: () => {},
+});
+
+const CartProvider = ({ children }) => {
+  const addItemtoCartHandler = () => {};
+
+  const removeItemFromCartHandler = (id) => {};
+
+  return (
+    <CartContext.Provider
+      value={{
+        items,
+        totalAmount,
+        addItem: addItemtoCartHandler,
+        removeItem: removeItemFromCartHandler,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+export default CartProvider;
