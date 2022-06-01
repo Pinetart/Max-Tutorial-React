889a18a20d8bda75de36680a0dd44d25296a2c79
Zane Birkett <axelbirkett@gmail.com>
2022-06-01 13:42:36 -0400

    How To (Not) Connect To A Database

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
-import React, { useState } from "react";
-import Button from "./components/UI/Button/Button";
+import React from 'react';
 
-import "./App.css";
+import MoviesList from './components/MoviesList';
+import './App.css';
 
 function App() {
-  const [showParagraph, setShowParagraph] = useState(false);
-  const toggleParagraphHandler = () => {
-    setShowParagraph((prevShowParagraph) => !prevShowParagraph);
-    // setShowParagraph(!showParagraph);
-  };
+  const dummyMovies = [
+    {
+      id: 1,
+      title: 'Some Dummy Movie',
+      openingText: 'This is the opening text of the movie',
+      releaseDate: '2021-05-18',
+    },
+    {
+      id: 2,
+      title: 'Some Dummy Movie 2',
+      openingText: 'This is the second opening text of the movie',
+      releaseDate: '2021-05-19',
+    },
+  ];
 
   return (
-    <div className="app">
-      <h1>Hi there!</h1>
-      {showParagraph && <p>This is new!</p>}
-      <Button onClick={toggleParagraphHandler}>Show Paragraph!</Button>
-    </div>
+    <React.Fragment>
+      <section>
+        <button>Fetch Movies</button>
+      </section>
+      <section>
+        <MoviesList movies={dummyMovies} />
+      </section>
+    </React.Fragment>
   );
 }
 
