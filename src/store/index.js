4a9ec13543ad9eb00b35697c256a982f3fb69596
Zane Birkett <axelbirkett@gmail.com>
2022-06-06 11:51:43 -0400

    Connecting Redux Toolkit State

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,9 @@
-import { createStore } from "redux";
+import { createStore, configureStore } from "redux";
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = { counter: 0, showCounter: true };
 
-createSlice({
+const counterSlice = createSlice({
   name: "counter",
   initialState,
   reducers: {
@@ -22,44 +22,9 @@ createSlice({
   },
 });
 
-const counterReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case "increment":
-      // state.counter++
-      //NEVER do the above, would be changing state snapshot
-      return {
-        ...state,
-        counter: state.counter + 1,
-        //   showCounter: state.showCounter,
-      };
-    case "decrement":
-      return {
-        ...state,
-        counter: state.counter - 1,
-        //   showCounter: state.showCounter,
-      };
-    case "increaseby5":
-      return {
-        ...state,
-        counter: state.counter + action.value,
-        // showCounter: state.showCounter,
-      };
-    case "clear":
-      return {
-        ...state,
-        counter: 0,
-      };
-    case "toggle":
-      return {
-        ...state,
-        showCounter: !state.showCounter,
-        // counter: state.counter,
-      };
-    default:
-      return state;
-  }
-};
-
-const store = createStore(counterReducer);
+const store = configureStore({
+  //   reducer: { counter: counterSlice.reducer },
+  reducer: counterSlice.reducer,
+});
 
 export default store;
