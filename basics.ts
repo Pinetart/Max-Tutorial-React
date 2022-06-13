169d6f0c7addd9ecaa86fd862779d2c91389e6dc
Zane Birkett <axelbirkett@gmail.com>
2022-06-13 13:16:34 -0400

    Working with Array & Object Types

--- basics.ts
+++ basics.ts
@@ -9,3 +9,7 @@ let userName: string = "zbirkett";
 let isInstructor: boolean = false;
 
 //Complex
+
+let hobbies: string[] = ["dog", "cat", "mouse"];
+let person: { name: string; age: number } = { name: "Zane", age: 24 };
+let people: { name: string; age: number }[] = [{ name: "Zane", age: 24 }];
