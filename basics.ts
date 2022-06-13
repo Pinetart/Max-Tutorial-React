c0ea7d1193b4ec1bfd517d79d1db8a19cfa3e0f6
Zane Birkett <axelbirkett@gmail.com>
2022-06-13 13:12:26 -0400

    Exploring the Base Types

---
+++ basics.ts
@@ -0,0 +1,11 @@
+// Primitives: number, string, boolean
+// Complex: array, objects
+// Function types, parameters
+
+//Primitives
+
+let age: number = 12.5;
+let userName: string = "zbirkett";
+let isInstructor: boolean = false;
+
+//Complex
