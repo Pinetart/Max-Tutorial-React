ac87b966fc4446bbd6349f24f8077e7ede71e0dc
Zane Birkett <axelbirkett@gmail.com>
2022-06-13 14:33:16 -0400

    Adding a Data Model

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import Todo from "../models/todo";
 
-const Todos: React.FC<{ items: string[] }> = ({ items }) => {
+const Todos: React.FC<{ items: Todo[] }> = ({ items }) => {
   return (
     <ul>
       {items.map((item) => (
-        <li key={item}></li>
+        <li key={item.id}>{item.text}</li>
       ))}
     </ul>
   );
