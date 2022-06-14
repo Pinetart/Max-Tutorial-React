1d7d5793b7507ff7c9760ed40f00a58e64f96809
Zane Birkett <Zane.birkett@houstydesigns.com>
2022-06-14 09:49:57 -0400

    Grouping Tests Together With Test Suites

--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,14 +1,16 @@
 import Greeting from "./Greeting";
 import { render, screen } from "@testing-library/react";
 
-test("renders hello world as a test", () => {
-  // Arrange
-  render(<Greeting />);
+describe("Greeting component", () => {
+  test("renders hello world as a test", () => {
+    // Arrange
+    render(<Greeting />);
 
-  // Act
-  // Nothing
+    // Act
+    // Nothing
 
-  // Assert
-  const helloWorldElement = screen.getByText("Hello World", { exact: false });
-  expect(helloWorldElement).toBeInTheDocument;
+    // Assert
+    const helloWorldElement = screen.getByText("Hello World", { exact: false });
+    expect(helloWorldElement).toBeInTheDocument;
+  });
 });
