22ae25ea4703f1dbca42008fe626ba17331f27fa
Zane Birkett <Zane.birkett@houstydesigns.com>
2022-06-14 10:41:21 -0400

    Working With Mocks

--- src/components/Async.test.js
+++ src/components/Async.test.js
@@ -3,6 +3,10 @@ import { render, screen } from "@testing-library/react";
 
 describe("Async component", () => {
   test("Renders posts if request succeeds", async () => {
+    window.fetch = jest.fn();
+    window.fetch.mockResolvedValueOnce({
+      json: async () => [{ id: "p1", title: "First post" }],
+    });
     // Arrange
     render(<Async />);
 
@@ -11,7 +15,7 @@ describe("Async component", () => {
 
     // Assert
     const listItemElements = await screen.findAllByRole("listitem");
-    
+
     expect(listItemElements).not.toHaveLength(0);
   });
 });
