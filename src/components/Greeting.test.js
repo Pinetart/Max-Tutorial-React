b486e9bfd511f7fc1eb0ccc1bdc1dd987a0a961c
Zane Birkett <Zane.birkett@houstydesigns.com>
2022-06-14 10:06:37 -0400

    Testing User Interaction & State

--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,4 +1,5 @@
 import Greeting from "./Greeting";
+import userEvent from "@testing-library/user-event";
 import { render, screen } from "@testing-library/react";
 
 describe("Greeting component", () => {
@@ -12,5 +13,47 @@ describe("Greeting component", () => {
     // Assert
     const helloWorldElement = screen.getByText("Hello World", { exact: false });
     expect(helloWorldElement).toBeInTheDocument;
+  });
+
+  test("renders its good to see you if button was not clicked", () => {
+    // Arrange
+    render(<Greeting />);
+
+    // Act
+    // Nothing
+
+    // Assert
+    const goodToSeeYouElement = screen.queryByText("good to see you", {
+      exact: false,
+    });
+    expect(goodToSeeYouElement).toBeInTheDocument;
+  });
+
+  test("renders changed if button was clicked", () => {
+    // Arrange
+    render(<Greeting />);
+
+    // Act
+    const buttonElement = screen.getByRole("button");
+    userEvent.click(buttonElement);
+
+    // Assert
+    const changedElement = screen.queryByText("changed", { exact: false });
+    expect(changedElement).toBeInTheDocument;
+  });
+
+  test("paragraph should not be visible when button is clicked", () => {
+    // Arrange
+    render(<Greeting />);
+
+    // Act
+    const buttonElement = screen.getByRole("button");
+    userEvent.click(buttonElement);
+
+    // Assert
+    const goodToSeeYouElement = screen.queryByText("good to see you", {
+      exact: false,
+    });
+    expect(goodToSeeYouElement).toBeNull;
   });
 });
